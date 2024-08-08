c1a57c2d2bf3521b5d80a28269f9d7dbb1a887ad
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-08-08 20:30:12 +0545

    token and repo

---
+++ book-network/src/main/java/com/kabin/book/user/Token.java
@@ -0,0 +1,32 @@
+package com.kabin.book.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Token {
+
+
+    @Id
+    @GeneratedValue
+    private String id;
+
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId" , nullable = false)
+    private User user;
+
+}
